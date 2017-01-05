34261931e38f625354094efe313d751782174979
BauRo <roy@bauro.io>
2017-01-05 21:58:40 +0100

    added configurable healthcheck

--- demo/index.php
+++ demo/index.php
@@ -1,5 +1,8 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $configuration = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . "/config/parameters.yml"));
@@ -18,6 +21,10 @@ $app->get('/', function () use ($app) {
 
     $response = ($app['apiClient'])->request('GET', $app['parameters']['dateApiService']);
     $date = json_decode($response->getBody()->getContents());
+
+    if (!isset($app['parameters']['healthy'])) {
+        return new Response('service is not healthy', 500);
+    }
 
     return $app['twig']->render('demo.twig', [
         'title' => $app['parameters']['title'],
