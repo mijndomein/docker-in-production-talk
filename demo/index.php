24d9c8568460ca2290294c72e032c07bdd6179f7
BauRo <roy@bauro.io>
2017-01-05 21:37:49 +0100

    added second api service

--- demo/index.php
+++ demo/index.php
@@ -4,17 +4,26 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $configuration = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . "/config/parameters.yml"));
 
+$guzzleClient = new GuzzleHttp\Client();
+
 $app = new Silex\Application();
 $app['parameters'] = $configuration['parameters'];
+$app['apiClient'] = $guzzleClient;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__ . '/views',
 ]);
 
 $app->get('/', function () use ($app) {
+    $response = ($app['apiClient'])->request('GET', 'http://api.service.consul');
+    /**
+     * @var \GuzzleHttp\Psr7\Response $response
+     */
+    $date = json_decode($response->getBody()->getContents());
+
     return $app['twig']->render('demo.twig', [
         'title' => $app['parameters']['title'],
-        'subTitle' => $app['parameters']['subTitle']
+        'subTitle' => $app['parameters']['subTitle'] . ' ' . $date->date
     ]);
 });
 
