1b2d0344c754de02913f53153a3645a07a5d2d91
BauRo <roy@bauro.io>
2017-02-14 15:20:00 +0100

    add timeout

--- demo/index.php
+++ demo/index.php
@@ -1,5 +1,8 @@
 <?php
 
+use GuzzleHttp\Exception\RequestException;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $configuration = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . "/config/parameters.yml"));
@@ -16,12 +19,20 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [
 
 $app->get('/', function () use ($app) {
 
-    $response = ($app['apiClient'])->request('GET', $app['parameters']['dateApiService']);
-    $date = json_decode($response->getBody()->getContents());
+    try {
+        $response = ($app['apiClient'])->request('GET', $app['parameters']['dateApiService'], ['timeout' => 2]);
+        $content = json_decode($response->getBody()->getContents());
+        $date = $content->date;
+    } catch (RequestException $e) {
+        $date = null;
+    }
+    if (!isset($app['parameters']['healthy']) || $app['parameters']['healthy'] !== "yes") {
+        return new Response('service is not healthy', 500);
+    }
 
     return $app['twig']->render('demo.twig', [
         'title' => $app['parameters']['title'],
-        'subTitle' => $app['parameters']['subTitle'] . ' ' . $date->date
+        'subTitle' => $app['parameters']['subTitle'] . ' ' . $date
     ]);
 });
 
